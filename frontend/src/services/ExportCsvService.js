525644361833febac9d85089253d615de2402469
fabuzard <44804900+fabuzard@users.noreply.github.com>
2025-08-28 11:16:28 +0700

    Fixing broken url from export csv

--- frontend/src/services/ExportCsvService.js
+++ frontend/src/services/ExportCsvService.js
@@ -1,7 +1,16 @@
-const API_URL = "https://inventory-backend-fabuzard2-b3f3496681aa.herokuapp.com:8080"; 
+const API_URL = "https://inventory-backend-fabuzard2-b3f3496681aa.herokuapp.com"; 
+
+function getAuthHeaders() {
+  const token = localStorage.getItem("token");
+  return {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  };
+}
 
 export const handleExportCSV = async () => {
-  const res = await fetch(`${API_URL}/products/export`);
+  const res = await fetch(`${API_URL}/products/export`,getAuthHeaders());
   const blob = await res.blob();
   const url = window.URL.createObjectURL(blob);
 
